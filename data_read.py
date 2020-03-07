eb2468bd44c072567bbf3da5e84b15273ff4fa11
LUNDR <lundrm@gmail.com>
2020-03-07 08:50:18 +0100

    added files to scrape expedia London-Zurich, place in s3 and to then read back into python session

---
+++ data_read.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  1 18:51:53 2020
+
+@author: lundr
+"""
+
+
+import boto3
+import pandas as pd
+from io import StringIO
+import numpy as np
+from datetime import time
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+from os import listdir
+from os.path import isfile, join
+
+from config import ACCESS_KEY,SECRET_KEY
+
+# bespoke functions
+
+def make_dep_time(x):
+    
+    """ function to extract arrival time and convert to datetime object
+        args: string of flight time
+    
+    """ 
+    
+    if 'pm' in x.split('-',1)[0]:
+        a = x.split('pm',1)[0]
+        
+        if int(a.split(':')[0])>11:
+            hour = int(a.split(':')[0])
+        else:
+            hour= int(a.split(':')[0])+12
+        b = time(hour=hour,minute=int(a.split(':')[1]))
+    else:
+        a = x.split('am',1)[0]
+        b = time(hour=int(a.split(':')[0]),minute=int(a.split(':')[1] ))
+    return b
+
+def make_arr_time(x):
+    
+    """ function to extract arrival time and convert to datetime object
+        args: string of flight time
+    
+    """ 
+    
+    k = x.split('-',1)[1]
+    if 'pm' in k :
+        a = k.split('pm',1)[0]
+    else:
+        a = k.split('am',1)[0]
+    b = time(hour=int(a.split(':')[0]),minute=int(a.split(':')[1] ))
+    return b
+
+def ListFiles(client,_BUCKET_NAME,_PREFIX):
+    
+    """List files in specific S3 URL"""
+    
+    response = client.list_objects(Bucket=_BUCKET_NAME, Prefix=_PREFIX)
+    for content in response.get('Contents', []):
+        yield content.get('Key')
+
+
+if __name__ == "__main__":
+
+    # create list of filenames
+    
+    s3 = boto3.client(
+    "s3",
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
+    )
+    
+    objt = s3.list_objects(Bucket='retailscrapes')
+    
+    
+    _BUCKET_NAME = 'retailscrapes'
+    _PREFIX = 'expedia/'
+
+
+    file_list = ListFiles(s3,_BUCKET_NAME,_PREFIX)
+    
+    files=[]
+    for f in file_list:
+        files.append(f)
+    
+    files = [f for f in files if '.txt' in f]
+    
+    df = pd.DataFrame(columns = ['date', 'airline','route','time','price','extraction_date'])
+    
+    for i in files:
+        
+      
+        obj = s3.get_object(Bucket = _BUCKET_NAME, Key = i)
+        body = obj['Body'].read()
+        
+        s=str(body,'utf-8')
+        
+        data = StringIO(s) 
+        
+        temp = pd.read_csv(data, sep='\t')
+        
+        df = df.append(temp, ignore_index = True)
+    
+    
+    df = df.loc[df['price'].notnull()]
+    
+    df.price = df.price.map(lambda x : float(x.split('$')[1]))
+    
+    
+    
+    # transform variables into dat/time
+    df['dep_time'] = df.time.map(lambda x : make_dep_time(x))
+    df['arr_time'] = df.time.map(lambda x : make_arr_time(x))
+    df['flt_date'] =df.date.map(lambda x : datetime.strptime(x,'%m-%d-%Y'))
+    df['scrape_date'] =pd.to_datetime(df['extraction_date'])
+    
+    df['day_of_week'] = df.flt_date.map(lambda x: x.weekday())
+    df['days_from_scrape'] = df.flt_date - df.scrape_date
+    df['days_from_scrape']= df.days_from_scrape.map(lambda x: x.days)
+    
+   
---
+++ expedia.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 20:02:47 2020
+
+@author: lundr
+"""
+
+
+import pandas as pd
+from datetime import datetime
+from datetime import date
+import boto3
+ 
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+import pandas as pd
+import time
+
+st = datetime.now()
+    
+
+
+days = list(range(1,32))
+month = [3,4,5,6,7,8,9,10,11,12]
+year = 2020
+
+
+df = pd.DataFrame(columns = ['date', 'airline','route','time','price'])
+for j in month:
+    
+    print('month:'+ str(j))
+
+    for i in days:
+      
+        try:
+            if  datetime(year, j,i) < datetime.now():
+                pass
+            else:
+            
+                print('day:'+ str(i))
+                
+                if len(str(i))==1:
+                    a='0'+str(i)
+                else:
+                    a = str(i)
+                    
+                    
+                if len(str(j))==1:
+                    m='0'+str(j)
+                else:   
+                    m = str(j)
+                    
+                date_stamp = m +'%2F'+ a+ '%2F'+ str(year)
+                date_clean = m+"-"+a+"-"+str(year)
+                
+                dep = 'London'
+                arr = 'Zurich'
+                
+                
+                # london - zurich
+                u = "https://www.expedia.com/Flights-Search?flight-type=on&starDate=" + date_stamp +"&mode=search&trip=oneway&leg1=from%3A" + dep+"%2C+England%2C+UK+%28LON-All+Airports%29%2Cto%3A"+arr+"%2C+Switzerland+%28ZRH%29%2Cdeparture%3A" + date_stamp +"TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY"
+             
+              
+                
+                #https://www.expedia.com/Flights-Search?flight-type=on&starDate=03%2F02%2F2020&mode=search&trip=oneway&leg1=from%3ALondon%2C+England%2C+UK+%28LON-All+Airports%29%2Cto%3AZurich%2C+Switzerland+%28ZRH%29%2Cdeparture%3A03%2F02%2F2020TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY
+              
+                
+                driver = webdriver.Chrome('C:/Users/lundr/Downloads/chromedriver_win32/chromedriver.exe')
+                driver.get(u)
+                time.sleep(10)
+                
+                
+               
+                # click x to close pop up
+                try:
+                    driver.find_element(By.XPATH,'//img[@class = " needsclick"]').click()
+                except:
+                    pass
+                
+                
+                
+                
+                prices = driver.find_elements_by_xpath('//span[@class = "full-bold no-wrap"]')
+                
+                departure = driver.find_elements_by_xpath('//span[@class  = "medium-bold"]')
+                airline = driver.find_elements_by_xpath('//span[@data-test-id  = "airline-name"]')
+                route = driver.find_elements_by_xpath('//div[@class  = "secondary-content no-wrap"]')
+                
+                
+        
+            
+                
+                for k in range(len(prices)):
+                  
+                   df2 = pd.DataFrame([[date_clean, airline[k+1].text,route[k].text, departure[k].text,prices[k].text]], columns = ['date','airline','route','time','price'])
+                   print(df2)
+                   df = df.append(df2, ignore_index = True)
+                
+                
+                
+                print('length df:' + str(df.shape[0]))
+                time.sleep(10)
+                
+                driver.close()
+                
+        except:
+                pass
+df['extraction_date'] = datetime.now()
+
+
+
+write_path='expedia_data/expedia.com_pm'+dep+"_"+arr+str(date.today())+'.txt'
+df.to_csv(write_path, sep='\t', index=False)
+    
+### send data to S3 bucket    
+
+from config import ACCESS_KEY,SECRET_KEY
+
+s3 = boto3.client(
+"s3",
+aws_access_key_id=ACCESS_KEY,
+aws_secret_access_key=SECRET_KEY
+)
+bucket_resource = s3
+
+filename = write_path
+
+bucket_resource.upload_file(
+Bucket = 'retailscrapes',
+Filename=filename,
+Key=filename
+)
+
+
+print(datetime.now() - st)
