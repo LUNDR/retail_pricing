4da475a48cf8b11a2049561e65a6eed5b716f548
LUNDR <lundrm@gmail.com>
2020-03-29 19:25:02 +0100

    corrected indentation issues

--- selenium_dino2.py
+++ selenium_dino2.py
@@ -4,6 +4,7 @@ from datetime import date
 import time
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from pyvirtualdisplay import Display
 import boto3
 import os
@@ -61,44 +62,43 @@ df = pd.DataFrame(columns = ['date', 'airline','route','time','price'])
 
 
 with webdriver.Chrome(chromedriver_path, options=options) as driver:
-	for day, month, year in itertools.product(dayList, monthList, yearList):
+    for day, month, year in itertools.product(dayList, monthList, yearList):
         try:
-    		if datetime(year, month,day) > datetime.now():
-    			d = f"{day:02d}"
-    			m = f"{month:02d}"
-    			y = f"{year:04d}"
+            if datetime(year, month,day) > datetime.now():
+                d = f"{day:02d}"
+                m = f"{month:02d}"
+                y = f"{year:04d}"
     			
-    			date_stamp = m +'%2F'+ d + '%2F'+ y
-    			date_clean = m + "-" + d + "-" + y
-    
-    			dep = 'London'
-    			arr = 'Zurich'
-    			
-    			# london - zurich
-    			u = "https://www.expedia.com/Flights-Search?flight-type=on&starDate=" + date_stamp +"&mode=search&trip=oneway&leg1=from%3A" + dep+"%2C+England%2C+UK+%28LON-All+Airports%29%2Cto%3A"+arr+"%2C+Switzerland+%28ZRH%29%2Cdeparture%3A" + date_stamp +"TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY"
-    
-    			driver.get(u)      
-    			time.sleep(10)
-    					
-    			# click x to close pop up
-    			try:
-    				driver.find_element(By.XPATH,'//img[@class = " needsclick"]').click()
-    			except:
-    				pass
-    			
-    			prices = driver.find_elements_by_xpath('//span[@class = "full-bold no-wrap"]')
-    			print(prices)
-    			departure = driver.find_elements_by_xpath('//span[@class  = "medium-bold"]')
-    			airline = driver.find_elements_by_xpath('//span[@data-test-id  = "airline-name"]')
-    			route = driver.find_elements_by_xpath('//div[@class  = "secondary-content no-wrap"]')
-    			
-    			for k in range(len(prices)):
-    				df2 = pd.DataFrame([[date_clean, airline[k+1].text,route[k].text, departure[k].text,prices[k].text]], columns = ['date','airline','route','time','price'])
-    				print(df2)
-    				df = df.append(df2, ignore_index = True)
-    			print('length df:' + str(df.shape[0]))
-    			time.sleep(10)
-		except:
+                date_stamp = m +'%2F'+ d + '%2F'+ y
+                date_clean = m + "-" + d + "-" + y
+                dep = 'London'
+                arr = 'Zurich'
+			
+        			# london - zurich
+                u = "https://www.expedia.com/Flights-Search?flight-type=on&starDate=" + date_stamp +"&mode=search&trip=oneway&leg1=from%3A" + dep+"%2C+England%2C+UK+%28LON-All+Airports%29%2Cto%3A"+arr+"%2C+Switzerland+%28ZRH%29%2Cdeparture%3A" + date_stamp +"TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY"
+        
+                driver.get(u)      
+                time.sleep(5)
+        					
+        			# click x to close pop up
+                try:
+                    driver.find_element(By.XPATH,'//img[@class = " needsclick"]').click()
+                except:
+                    pass
+                
+                prices = driver.find_elements_by_xpath('//span[@class = "full-bold no-wrap"]')
+                print(prices)
+                departure = driver.find_elements_by_xpath('//span[@class  = "medium-bold"]')
+                airline = driver.find_elements_by_xpath('//span[@data-test-id  = "airline-name"]')
+                route = driver.find_elements_by_xpath('//div[@class  = "secondary-content no-wrap"]')
+			
+                for k in range(len(prices)):
+                    df2 = pd.DataFrame([[date_clean, airline[k+1].text,route[k].text, departure[k].text,prices[k].text]], columns = ['date','airline','route','time','price'])
+                    print(df2)
+                    df = df.append(df2, ignore_index = True)
+                    print('length df:' + str(df.shape[0]))
+                time.sleep(10)
+        except:
                 pass		
 
 df['extraction_date'] = datetime.now()
